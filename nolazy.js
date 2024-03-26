f6de7549403031905296dffd19396728ece208f8
Patty-OFurniture <127074553+Patty-OFurniture@users.noreply.github.com>
2024-03-25 21:49:33 -0400

    More cleanup on success

--- nolazy.js
+++ nolazy.js
@@ -15,17 +15,20 @@
 
     for (i = 0; i < images.length; ++i) {
 
+		var img = images[i];
 		// patch: overwrite src with the largest option from data-srcset
-		if (images[i].hasAttribute("data-srcset"))
+		if (img.hasAttribute("data-srcset"))
 		{
-			var img = images[i];
 			try {
 				var sizes = img.attributes["data-srcset"].value.split(',').map(i => ({ src: i.trim().split(' ')[0], size: i.trim().split(' ')[1]}));
 				var size = sizes.sort( (a,b) => (a.size.length == b.size.length) ? a.size < b.size : a.size.length < b.size.length)[0];
 				img.src = size.src;
+				// if successful, clean up attributes
 				img.removeAttribute("data-srcset");
 				// console.log(sizes);
 				console.log("largest image: " + size.size);
+				if (img.hasAttribute("data-src"))
+					img.removeAttribute("data-src");
 				var classes = img.className.split(' ');
 				// TODO: refactor, and why was it "++y"??
 				// remove lazy and pending classes that may prevent showing the image
@@ -43,19 +46,19 @@
 		}
 		// /patch
 
-        for (x = 0; x < images[i].attributes.length; x++) {
-            attr = images[i].attributes[x];
+        for (x = 0; x < img.attributes.length; x++) {
+            attr = img.attributes[x];
 
             if (rxdata.test(attr.nodeName)) {
 
                 console.log(attr.nodeName + " = " + attr.nodeValue);
                 if (rxext.test(attr.nodeValue)) {
-                    images[i].src = attr.nodeValue;
-                    classes = images[i].className.split(' ');
+                    img.src = attr.nodeValue;
+                    classes = img.className.split(' ');
                     // remove lazy and pending classes that may prevent showing the image
                     for (y = 0; y < classes.length; y++) {
                         if (rxclass.test(classes[y])) {
-                            images[i].classList.remove(classes[y]);
+                            img.classList.remove(classes[y]);
                             console.log("removed class: " + classes[y]);
                         }
                     }
