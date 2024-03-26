b8cf5df1af8b7ad88f043c8f81baa3f538bedf1c
Patty-OFurniture <127074553+Patty-OFurniture@users.noreply.github.com>
2024-03-25 20:12:00 -0400

    If data-srcset exists, choose the largest file

--- nolazy.js
+++ nolazy.js
@@ -5,8 +5,8 @@
 
     var images  = document.querySelectorAll('img'),
         rxdata  = new RegExp('^data-'),
-        rxclass = new RegExp('.*\\b(lazy|pending)\\b.*'),
-        rxext   = new RegExp('\.(gif|jpe?g|png)$'),
+        rxclass = new RegExp('.*\\b(lazy|lazyload|pending)\\b.*'),
+        rxext   = new RegExp('\.(gif|jpe?g|png|webp|bmp)$'),
         i,
         x,
         y,
@@ -15,35 +15,54 @@
 
     for (i = 0; i < images.length; ++i) {
 
+		// patch: overwrite src with the largest option from data-srcset
+		if (images[i].hasAttribute("data-srcset"))
+		{
+			var img = images[i];
+			try {
+				var sizes = img.attributes["data-srcset"].value.split(',').map(i => ({ src: i.trim().split(' ')[0], size: i.trim().split(' ')[1]}));
+				var size = sizes.sort( (a,b) => (a.size.length == b.size.length) ? a.size < b.size : a.size.length < b.size.length)[0];
+				img.src = size.src;
+				img.removeAttribute("data-srcset");
+				// console.log(sizes);
+				console.log("largest image: " + size.size);
+				var classes = img.className.split(' ');
+				// TODO: refactor, and why was it "++y"??
+				// remove lazy and pending classes that may prevent showing the image
+				for (y = 0; y < classes.length; y++) {
+					if (rxclass.test(classes[y])) {
+						img.classList.remove(classes[y]);
+						console.log("removed class: " + classes[y]);
+					}
+				}
+			}
+			catch (e) {
+				console.error(e);
+			}
+			continue;
+		}
+		// /patch
+
         for (x = 0; x < images[i].attributes.length; x++) {
             attr = images[i].attributes[x];
 
             if (rxdata.test(attr.nodeName)) {
 
                 console.log(attr.nodeName + " = " + attr.nodeValue);
-
                 if (rxext.test(attr.nodeValue)) {
                     images[i].src = attr.nodeValue;
-
                     classes = images[i].className.split(' ');
-
-                    // remove lazy and pendign classes that may prevent showing the image
-                    for (y = 0; y < classes.length; ++y) {
-
+                    // remove lazy and pending classes that may prevent showing the image
+                    for (y = 0; y < classes.length; y++) {
                         if (rxclass.test(classes[y])) {
                             images[i].classList.remove(classes[y]);
                             console.log("removed class: " + classes[y]);
                         }
-
                     }
 
-                    break;
+					break;
                 }
-
             }
-
         }
-
     }
-
 })();
